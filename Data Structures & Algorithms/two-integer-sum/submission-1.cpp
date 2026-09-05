d502aee62048e20a3d3bd0e4aa5f87bfcdd6c777
Arshdeep2026AIM <2026aim1004@iitrpr.ac.in>
2026-09-05 21:20:41 +0530

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int, int> index;
+        for (int i = 0; i < nums.size(); i++) {
+            if (index.find(target - nums[i]) != index.end()) {
+                return {index[target - nums[i]], i};
+            }
+            index.insert({nums[i], i});
+        }
+        return {};
+    }
+};
